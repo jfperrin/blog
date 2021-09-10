cc0abb3c6320d8f71196828a165ffd3a655f68d8
Jeff <jfperrin@artprice.com>
2021-09-10 14:50:55 +0200

    Edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,32 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def create
-    user = User.create!(user_params)
-    session[:user_id] = user.id
-    redirect_to users_url
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   private
